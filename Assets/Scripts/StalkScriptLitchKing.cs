2a2ab8adb01fbc623f80ccbb4e1d32d16fe4caa8
DerEndergeist <rgausw@gmail.com>
2021-12-22 14:59:19 +0100

    Added LitchKing that follows
    Created a NavMesh for the Castle, its surrounding and the plain.
    Created a Capsule called Litch king. it uses the navMesh to Navigate and has a Script that allows it to chase the player.
    Has some problems with the Terrain tho

---
+++ Assets/Scripts/StalkScriptLitchKing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class StalkScriptLitchKing : MonoBehaviour
+{
+    public Transform Destination = null;
+    private NavMeshAgent ThisAgent = null;
+
+    void Awake()
+    {
+        ThisAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        ThisAgent.SetDestination(Destination.position);
+    }
+}
