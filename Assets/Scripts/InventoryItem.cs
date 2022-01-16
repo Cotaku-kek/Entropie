3dc039fa4bb45533365babd5feee5eeafa434795
polymeros1856 <i.paslack@freenet.de>
2022-01-16 13:29:48 +0100

    Error Jelöst

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public interface InventoryItem : MonoBehaviour
+{
+    string Name { get; }
+
+    Sprite Image { ger; }
+
+    void OnPickup();
+
+}
+
+public class InventoryEventArgs : InventoryEventArgs
+{
+    public InventoryEventArgs(IInventoryItem item)
+    {
+        item = item;
+    }
+
+    public IInventoryItem Item;
+}
