6dbf5536fbcdf078d9fe691f760981b4740381c9
DerEndergeist <rgausw@gmail.com>
2022-01-17 23:48:51 +0100

    Added a raven that gets panicked by monster and player
    The raven just starts jumping high.
    I don't know why.
    it was supposed to fly

---
+++ Assets/Scripts/RavensFlight.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RavensFlight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player = null;
+    public Transform monster = null;
+    public float sightRange = 10;
+    private bool isFrightened;
+    void Start()
+    {
+    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFrightened)
+        {
+            Vector3 a = transform.position;
+            a.y += 0.1f;
+            transform.position = a;
+        }
+        if (Vector3.Distance(transform.position, player.transform.position)<sightRange) 
+        {
+            AlertMonster();
+            FlyAway(); 
+        }
+        if (Vector3.Distance(transform.position, monster.transform.position) < sightRange) FlyAway();
+
+    }
+    private void FlyAway()
+    {
+        isFrightened = true;
+    }
+    private void AlertMonster()
+    {
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color=Color.red;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
