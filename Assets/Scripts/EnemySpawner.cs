889730de1b886e897dbf558c487e79262040223e
Cotaku-kek <c.nichita@web.de>
2022-03-01 16:16:24 +0100

    Spawn random enemy works, but other enemies have to be removed from the scene

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] myObjects;
+
+    public virtual void Start()
+    {
+        int randomIndex = Random.Range(0, myObjects.Length);
+        Vector3 randomSpawnPos = new Vector3(Random.Range(0, myObjects.Length - 1), 5, Random.Range(0, myObjects.Length - 1));
+
+        Instantiate(myObjects[UnityEngine.Random.Range(0, myObjects.Length - 1)]);
+    }
+
+}
