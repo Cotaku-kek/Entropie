cb5fcd197bb3eede7de637b85410773ee1558324
Cotaku-kek <c.nichita@web.de>
2022-03-02 11:09:35 +0100

    Spawn testing

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -9,9 +9,10 @@ public class EnemySpawner : MonoBehaviour
     public virtual void Start()
     {
         int randomIndex = Random.Range(0, myObjects.Length);
-        Vector3 randomSpawnPos = new Vector3(Random.Range(0, myObjects.Length - 1), 5, Random.Range(0, myObjects.Length - 1));
+        Vector3 randomSpawnPos = new Vector3(Random.Range(-5, -10), 3, Random.Range(1, 7));
 
         Instantiate(myObjects[UnityEngine.Random.Range(0, myObjects.Length - 1)]);
+        Debug.Log("Random enemy spawned");
     }
 
 }
