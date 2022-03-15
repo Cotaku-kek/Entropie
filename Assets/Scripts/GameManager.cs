518e3185ee550620eb281884f19751807c822f50
Cotaku-kek <c.nichita@web.de>
2022-03-15 21:12:30 +0100

    Bug fixing, create GameManger Script, enemy is invinsible, after few sec enemy spawns with sound

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float timeToSpawn;
+    public float timeToDespawn;
+
+    public AudioSource bell;
+
+    public AudioSource background;
+
+    public GameObject enemy;
+    public GameObject player;
+
+    public GameObject enemyReference;
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyTimer());
+    }
+
+    void Update()
+    {
+        
+    }
+   
+    IEnumerator SpawnEnemyTimer()
+    {
+        yield return new WaitForSeconds(timeToSpawn);
+        SpawnEnemy();
+    }
+
+    void DespawnEnemy()
+    {
+        Destroy(enemyReference);
+        StartCoroutine(SpawnEnemyTimer());
+    }
+
+    IEnumerator DespawnEnemyTimer()
+    {
+        yield return new WaitForSeconds(timeToDespawn);
+        DespawnEnemy();
+    }
+
+    void SpawnEnemy()
+    {
+        bell.Play();
+        Vector3 spawnPos = player.transform.position + Random.insideUnitSphere * 10;
+        enemyReference = Instantiate(enemy, spawnPos, Quaternion.identity);
+
+        StartCoroutine(DespawnEnemyTimer());
+    }
+}
