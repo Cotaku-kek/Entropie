8284eec11cebd0f6f7281bae3fc5b6c64febf991
DerEndergeist <rgausw@gmail.com>
2022-03-25 17:15:39 +0100

    Monster Behaviour
    Added the different ways of behaviour and visibilities

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //Auskommentiert: Cristinas Code
 
-    public float timeToSpawn;
-    public float timeToDespawn;
+    /*public float timeToSpawn;
+    public float timeToDespawn;*/
+    public float timeBehaviourTick;
 
     public AudioSource bell;
-
     public AudioSource background;
 
     public GameObject[] enemy;
@@ -18,7 +19,9 @@ public class GameManager : MonoBehaviour
     public GameObject enemyReference;
     void Start()
     {
-        StartCoroutine(SpawnEnemyTimer());
+        //StartCoroutine(SpawnEnemyTimer());
+        SpawnEnemy();
+        StartCoroutine(BehaviourTickTimer());
     }
 
     void Update()
@@ -26,33 +29,83 @@ public class GameManager : MonoBehaviour
         
     }
    
-    IEnumerator SpawnEnemyTimer()
+    IEnumerator BehaviourTickTimer()
+    {
+        yield return new WaitForSeconds(timeBehaviourTick);
+        BehaviourTick();
+    }
+    void BehaviourTick()
+    {
+        int Choice = Random.Range(0, 100);
+        Debug.Log(Choice);
+        if (0 <= Choice&&Choice <= 45)
+        {
+            Hint();
+        }
+        else if (46 <= Choice && Choice <= 90)
+        {
+            Blink();
+        }
+        else if (91 <= Choice && Choice <= 100)
+        {
+            Hunt();
+        }
+
+    }
+    void Hint()
+    {
+        Debug.Log("Hint");
+        StartCoroutine(BehaviourTickTimer());
+    }
+    void Blink()
+    {
+        Debug.Log("Blink");
+        enemyReference.SetActive(true);
+        StartCoroutine(BlinkTickTimer());
+    }
+    IEnumerator BlinkTickTimer()
+    {
+        yield return new WaitForSeconds(1);
+        enemyReference.SetActive(false);
+        Debug.Log("BlinkEnded");
+        StartCoroutine(BehaviourTickTimer());
+    }
+    void Hunt()
+    {
+        Debug.Log("Hunt Starting");
+        bell.Play();
+        RenderSettings.skybox.SetColor("_SkyTint", Color.red);
+        enemyReference.SetActive(true);
+        StartCoroutine(BehaviourTickTimer());
+    }
+
+    /*IEnumerator SpawnEnemyTimer()
     {
         yield return new WaitForSeconds(timeToSpawn);
         SpawnEnemy();
-    }
+    }*/
 
-    void DespawnEnemy()
-    {
-        Destroy(enemyReference);
-        StartCoroutine(SpawnEnemyTimer());
-    }
+    /* void DespawnEnemy()
+     {
+         Destroy(enemyReference);
+         StartCoroutine(SpawnEnemyTimer());
+     }*/
 
-    IEnumerator DespawnEnemyTimer()
-    {
-        yield return new WaitForSeconds(timeToDespawn);
-        DespawnEnemy();
-    }
+    /* IEnumerator DespawnEnemyTimer()
+     {
+         yield return new WaitForSeconds(timeToDespawn);
+         DespawnEnemy();
+     }*/
 
     void SpawnEnemy()
     {
-        RenderSettings.skybox.SetColor("_SkyTint", Color.red);
-        int randomIndex = Random.Range(0, enemy.Length);
-        bell.Play();
-        Vector3 spawnPos = player.transform.position + Random.insideUnitSphere * 10;
-        enemyReference = Instantiate(enemy[randomIndex], spawnPos, Quaternion.identity);
 
-        StartCoroutine(DespawnEnemyTimer());
+        int randomIndex = Random.Range(0, enemy.Length);
+        Vector3 spawnPos = new Vector3(-66.1f,35.323f,-43.7f);
+        enemyReference = Instantiate(enemy[randomIndex], spawnPos, Quaternion.identity);
+        enemyReference.SetActive(false);
+
+       // StartCoroutine(DespawnEnemyTimer());
     }
 
     public static void UpdateEnvironment() {
