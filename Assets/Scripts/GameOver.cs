e0c91e5248d861715cd538551524eaa0dc5dde47
Cotaku-kek <c.nichita@web.de>
2022-03-26 17:00:24 +0100

    Start and GameOver works fine :)

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GameOver : MonoBehaviour
 {
 
+    public void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void LoadGame()
     {
         Debug.Log("Button is pressed");
         SceneManager.LoadScene("StartScreen");
     }
-
     
 }
