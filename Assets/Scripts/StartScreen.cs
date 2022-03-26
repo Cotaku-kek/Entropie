59f65cf30f1013d775d6e426aef44adfa925ed2c
Cotaku-kek <c.nichita@web.de>
2022-03-26 13:09:26 +0100

    StartScreen and GameOverScreen are included and connected (not perfect)

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log("Key is pressed");
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
