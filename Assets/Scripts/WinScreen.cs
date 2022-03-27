71f9ba0b81ce8bead2e5e305a5a16249a6888366
Cotaku-kek <c.nichita@web.de>
2022-03-27 16:54:51 +0200

    Included WinScreen

---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    public void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Button is pressed");
+        SceneManager.LoadScene("StartScreen");
+    }
+
+}
