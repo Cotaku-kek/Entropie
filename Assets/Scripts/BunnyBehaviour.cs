8ef4d96ad6accd34755e19fb84d0dfb0b5d87d15
DerEndergeist <rgausw@gmail.com>
2022-03-29 16:00:45 +0200

    Hints For WereBunny
    Only needs the sounds referenced in the SerializedFields and also needs to play stomping sounds when walking

---
+++ Assets/Scripts/BunnyBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyBehaviour : MonoBehaviour
+{
+    [SerializeField] AudioSource AudioScratch;
+    [SerializeField] AudioSource AudioBreath;
+
+    int ChanceScratches = 42;
+    int ChanceBreath = 58;
+
+    public void GiveHint()
+    {
+        int chance = Random.Range(1, 100);
+        if (chance <= ChanceScratches)
+        {
+            DoScratchesHint();
+        }
+        else { DoBreathHint(); }
+    }
+
+    private void DoBreathHint()
+    {
+        AudioScratch.Play();
+    }
+
+    private void DoScratchesHint()
+    {
+        AudioBreath.Play();
+    }
+}
